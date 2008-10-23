ffa2466aef4decb6c1d14393e83fdbf70a83aaa6
collin <collintmiller@gmail.com>
2008-10-22 19:05:03 -0500

    starting on url generation

--- jquery.resource.js
+++ jquery.resource.js
@@ -1,1 +1,36 @@
-
+;(function(_){
+  _.resource = function(name) {
+    var pluralized = Inflector.pluralize(name)
+      ,loc = window.location
+      ,url_base = loc.protocol+'//'+loc.hostname;
+    
+    function query_string(options) {
+      if(options == undefined) return "";
+      var q = "?", slot;
+      for(slot in options) q+= "&" + slot + '=' + options[slot];
+      return q;
+    }
+    
+    function path_with_options(fn) {
+      return function() {
+        var args = _.makeArray(arguments), options;
+        if(args.length > 1) options = args.pop();
+        return fn.call(this, args) + query_string(options);
+      };
+    };
+    
+    _[pluralized+'_path'] = function() {
+      return '/'+pluralized;
+    };
+    
+    _[pluralized+'_url'] = function() {
+      return url_base+'/'+pluralized;
+    };
+    
+    _[name+'_path'] = path_with_options(function(id) {
+      return _[pluralized+'_path']() + '/' + id;
+    });
+    
+    
+  }
+})(jQuery);
--- test.js
+++ test.js
@@ -1,85 +1,19 @@
-jQuery(function(_){  
-  _.template("interp", "#{msg}, #{who}!");
-  match("Hello, World!", function(){
-    return _.template('interp', {
-      msg: "Hello", who: "World"
-    });
-  });
- 
-  _.template("defer", "={interp} ={interp}") 
-  match("Hello, Tests! Hello, Tests!", function() {
-    return _.template('defer', {
-      msg: "Hello", who: "Tests"
-    });
+jQuery(function(_){
+  var loc = window.location;  
+  _.resource('book')
+  
+  match("/books", _.books_path);
+  match(loc.protocol+'//'+loc.hostname+"/books", _.books_url);
+  
+  match("/books/1", function() {
+    return _.book_path(1);
   });
   
-  _.template("pass", "={interp:jupiter} ={interp:mars}");
-  match("Goodbye, Jupiter! Hello, Mars!", function() {
-    return _.template('pass', {
-      jupiter: {msg:"Goodbye", who:"Jupiter"}
-      ,mars: {msg:"Hello", who:"Mars"}
-    });
+  match("/books/barnaby_jones", function() {
+    return _.book_path('barnaby_jones');
   });
   
-  _.template("late", "={?(template)}")
-  match("Hello, Nurse!", function() {
-    return _.template('late', {
-      template: 'interp', msg: "Hello", who: "Nurse"
-    });
+  match("/books/the_theif?&page=33", function() {
+    return _.book_path('the_theif', {page:33});
   });
-  
-  _.template("list", "={interp:us}")
-  match("Hello, Me!Hello, You!", function() {
-    return _.template('list', {
-      us: [{
-        msg:"Hello", who:"Me"
-      },{
-        msg:"Hello", who:"You"
-      }]
-    });
-  });
-  
-  _.template('print', "#{ln}\n");
-  _.template('expression', "=[print||ln<-lines]");
-  match("a\nb\nc\nd\n", function() {
-    return _.template('expression', {
-      lines: "a.b.c.d".split('.')
-    });
-  });
-  
-  _.template('fncall', "#{prop}");
-  match("woot", function() {
-    return _.template('fncall', {
-      prop: function() {return 'woot';}
-    });
-  });
-  
-  _.template('fncall_template', "={fncall:things}");
-  match('abcd', function() {
-    return _.template('fncall_template', {
-      things: function() {
-        return [{
-          prop:'a'
-        },{
-          prop:'b'
-        },{
-          prop:'c'
-        },{
-          prop:'d'
-        }];
-      }
-    })
-  });
-  
-  _.template('fncall_list_exp', "=[fncall||prop<-things]");
-  match('abcd', function() {
-    return _.template('fncall_list_exp', {
-      things: function() {
-        return "a b c d".split(/ /);
-      }
-    });
-  });
-  
-  assert(function(){return false;})
-  assert(function(){return biddy;})
 });
